55f8044dd11595009311e3e0daac3263185cf9c7
Matt <matt@primestudiosllc.com>
2014-03-20 11:54:29 -0400

    updated sensitivity

--- CapacitiveTouch.ino
+++ CapacitiveTouch.ino
@@ -6,13 +6,14 @@
  *
  * All signals are active low
 */
+int countLoop = 0;
 
-const int cap_left_in = 2;
+const int cap_left_in = 4;
 const int cap_shared = 3;
-const int cap_right_in = 4;
+const int cap_right_in = 2;
 
-const int cap_left_out = 8;
-const int cap_right_out = 9;
+const int cap_left_out = 9;
+const int cap_right_out = 10;
 const int ir_reach = 6;
 const int ir_back = 7;
 
@@ -25,10 +26,9 @@ CapacitiveSensor cap_right_sensor = CapacitiveSensor(cap_shared,cap_right_in);
 long cap_left_value = 0;
 long cap_right_value = 0;
 
-long cap_left_threshold = 200;
-long cap_right_threshold = 200;
+long cap_left_threshold = 75;
+long cap_right_threshold = 75;
 
-long cap_autocal_value = 5000;
 byte cap_samples = 25;
 
 void setup()                    
@@ -49,41 +49,44 @@ void setup()
 
 void loop()                    
 {
-  //long start = millis();
+  //countLoop = countLoop + 1;
   
   cap_left_value = cap_left_sensor.capacitiveSensor(cap_samples);
   cap_right_value = cap_right_sensor.capacitiveSensor(cap_samples);
+  /*Serial.println(cap_left_value);
+  Serial.println(cap_right_value);*/
 
   if(digitalRead(ir_back) == HIGH) {
-    //Serial.println("Not back...");
     digitalWrite(led_mirror, HIGH);
+    cap_left_sensor.set_CS_AutocaL_Millis(0xFFFFFFFF); //autocal off
+    cap_right_sensor.set_CS_AutocaL_Millis(0xFFFFFFFF); //autocal off
     if(cap_left_value > cap_left_threshold) { //paw on left sensor?
-      cap_left_sensor.set_CS_AutocaL_Millis(0xFFFFFFFF); //autocal off
       digitalWrite(cap_left_out, LOW);
     } else {
-      cap_left_sensor.set_CS_AutocaL_Millis(cap_autocal_value); //autocal on
       digitalWrite(cap_left_out, HIGH);
     }
     if(cap_right_value > cap_right_threshold) { //paw on right sensor?
-      cap_right_sensor.set_CS_AutocaL_Millis(0xFFFFFFFF); //autocal off
       digitalWrite(cap_right_out, LOW);
     } else {
-      cap_right_sensor.set_CS_AutocaL_Millis(cap_autocal_value); //autocal on
       digitalWrite(cap_right_out, HIGH);
     }
   } else {
      //Serial.println("BACK!");
+     //first thin, make sure sensors are HIGH
+     digitalWrite(cap_right_out, HIGH);
+     digitalWrite(cap_left_out, HIGH);
+     
      digitalWrite(led_mirror, LOW);
+     
      cap_left_sensor.reset_CS_AutoCal();
      cap_right_sensor.reset_CS_AutoCal();
   }
 /*
-  Serial.print(millis() - start); //check on performance in milliseconds
-  Serial.print("\t");
-
-  Serial.print(cap_left_value); //print sensor output 1
-  Serial.print("\t");
-  Serial.println(cap_right_value); //print sensor output 2
-  delay(50);
-*/  
+  if(countLoop > 10) {
+     Serial.print(cap_left_value); //print sensor output 1
+     Serial.print("\t");
+     Serial.println(cap_right_value); //print sensor output 2
+     countLoop = 0;
+  }
+*/
 }
